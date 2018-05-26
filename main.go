bc99b2829433f96f5845ffebd8c900e055fc6e2a
Jason Stallings <jason@stallin.gs>
2018-05-26 11:15:12 -0500

    Add uiprogress and gabs.

--- main.go
+++ main.go
@@ -8,35 +8,14 @@ import (
 	"net/http"
 	"sync"
 	"time"
+
+	"github.com/Jeffail/gabs"
+	"github.com/gosuri/uiprogress"
 )
-
-type Actor struct {
-	Id          int    `json:"id"`
-	Login       string `json:"login"`
-	Gravatar_id string `json:"gravatar_id"`
-	Url         string `json:"url"`
-	Avatar_url  string `json:"avatar_url"`
-}
-type Payload struct {
-	Ref           string `json:"ref"`
-	Ref_type      string `json:"ref_type"`
-	Master_branch string `json:"master_branch"`
-	Description   string `json:"description"`
-	Pusher_type   string `json:"pusher_type"`
-}
-
-type Event struct {
-	Id        string                 `json:"id"`
-	Type      string                 `json:"Type"`
-	CreatedAt string                 `json:"created_at"`
-	Actor     Actor                  `json:"actor"`
-	Payload   map[string]interface{} `json:"payload"`
-	Public    bool                   `json:"public"`
-}
 
 func check(e error) {
 	if e != nil {
-		panic(e)
+		log.Print(e)
 	}
 }
 
@@ -44,7 +23,12 @@ func getUrl(date time.Time) string {
 	return fmt.Sprintf("http://data.gharchive.org/%02d-%02d-%02d-%d.json.gz", date.Year(), int(date.Month()), date.Day(), date.Hour())
 }
 
-func decodeFromUrl(url string, channel chan Event, wg *sync.WaitGroup) {
+type incr func()
+
+func decodeFromUrl(url string, channel chan *gabs.Container, wg *sync.WaitGroup, done incr) {
+	defer done()
+	defer wg.Done()
+
 	resp, _ := http.Get(url)
 	defer resp.Body.Close()
 
@@ -54,39 +38,49 @@ func decodeFromUrl(url string, channel chan Event, wg *sync.WaitGroup) {
 
 	// Decode event.
 	for dec.More() {
-		var e Event
-		err := dec.Decode(&e)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if e.Actor.Login == "octalmage" {
-			channel <- e
-			fmt.Println(e)
+		parsed, err := gabs.ParseJSONDecoder(dec)
+		check(err)
+		name, _ := parsed.Path("actor.login").Data().(string)
+
+		if name == "octalmage" {
+			channel <- parsed
+			fmt.Println(name)
 		}
 	}
-	wg.Done()
 }
 
 func main() {
 	now := time.Now()
-	aYearAgo := now.AddDate(-1, 0, 0)
+	aYearAgo := now.AddDate(-1, -1, 0)
 
-	channel := make(chan Event)
+	channel := make(chan *gabs.Container)
 	wg := sync.WaitGroup{}
-	for hour := 0; hour <= 23; hour++ {
+
+	hours := 24
+	bar := uiprogress.AddBar(hours).AppendCompleted().PrependElapsed()
+	bar.PrependFunc(func(b *uiprogress.Bar) string {
+		return fmt.Sprintf("Fetching hours (%d/%d)", b.Current(), hours)
+	})
+
+	uiprogress.Start()
+	bar.Incr()
+	for hour := 0; hour <= hours-1; hour++ {
 		dateForUrl := time.Date(aYearAgo.Year(), aYearAgo.Month(), aYearAgo.Day(), hour, 0, 0, 0, aYearAgo.Location())
 		ghUrl := getUrl(dateForUrl)
 		wg.Add(1)
-		go decodeFromUrl(ghUrl, channel, &wg)
+		go decodeFromUrl(ghUrl, channel, &wg, func() {
+			bar.Incr()
+		})
 	}
 
 	go func() {
 		wg.Wait()
+		uiprogress.Stop()
 		close(channel)
 	}()
 
 	for item := range channel {
-		fmt.Println(item.Type)
+		fmt.Println(item)
 	}
 
 	// io.Copy(os.Stdout, uncompressed_resp)
